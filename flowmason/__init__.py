67b474b2dca0b3c4b0e5a80c4487819da707b594
smfsamir <f.samir@mail.utoronto.ca>
2023-10-02 16:01:01 -0700

    update

--- flowmason/__init__.py
+++ flowmason/__init__.py
@@ -1,1 +1,2 @@
 from .dag import conduct, cacheable
+from .inspector import load_artifact, load_latest_step_artifact
---
+++ flowmason/inspector.py
@@ -0,0 +1,18 @@
+import dill
+from typing import Tuple, Dict
+import json
+import os
+
+def load_latest_step_artifact(experiment_name: str):
+    # load the latest file. It will be run_####.json under
+    # outputs/{experiment_name}
+    # the latest one will have the highest number.
+    # code:
+    fname = max([f for f in os.listdir(os.path.join("outputs", experiment_name)) if f.startswith("run_")])
+    with open(os.path.join("outputs", experiment_name, fname), 'r') as f:
+        return json.load(f)
+
+def load_artifact(step: Tuple[str, Dict[str, str]]):
+    artifact_path = step[1]["cache_path"]
+    with open(artifact_path, 'rb') as f:
+        return dill.load(f)
