794df2ff8f8bfd4ef5ced6c79d11238b4b07ba54
Farhan Samir <f.samir@mail.utoronto.ca>
2024-02-19 18:58:42 -0800

    make load artifact with step name discoverable

--- flowmason/__init__.py
+++ flowmason/__init__.py
@@ -1,2 +1,2 @@
 from .dag import conduct, SingletonStep, MapReduceStep
-from .inspector import load_artifact, load_latest_steps
+from .inspector import load_artifact, load_latest_steps, load_artifact_with_step_name
