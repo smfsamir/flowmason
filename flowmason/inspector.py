c990a5bcfe35b0fbc499ecf2d45a91280b3e8506
Farhan Samir <f.samir@mail.utoronto.ca>
2024-02-19 18:54:30 -0800

    update

--- flowmason/inspector.py
+++ flowmason/inspector.py
@@ -2,7 +2,9 @@ import dill
 from typing import Tuple, Dict
 import json
 import os
+import loguru
 
+logger = loguru.logger
 def load_latest_steps(experiment_name: str):
     # load the latest file. It will be run_####.json under
     # outputs/{experiment_name}
@@ -16,3 +18,10 @@ def load_artifact(step: Tuple[str, Dict[str, str]]):
     artifact_path = step[1]["cache_path"]
     with open(artifact_path, 'rb') as f:
         return dill.load(f)
+
+def load_artifact_with_step_name(metadata, step_name):
+    for step in metadata:
+        if step[0] == step_name:
+            return load_artifact(step)
+    logger.error(f"Step {step_name} not found in metadata")
+    return -1
